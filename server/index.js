f531b2d851c474d198ce90dbf88ea74c08833013
Shivam <sriyanshshivam1@gmail.com>
2024-11-28 16:28:24 +0530

    Complete base code for index.js

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('Connected to MongoDB')).catch(err => console.log(err));
+
+app.use('/api', require('./routes/messages'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
