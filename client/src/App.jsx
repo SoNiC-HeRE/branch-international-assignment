73193b6180d0c779d7497c2bc2b0a290af67098c
Shivam <sriyanshshivam1@gmail.com>
2024-11-29 02:10:16 +0530

    Complete homepage and basic layout for the design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage/HomePage.jsx";
+import UserPage from "./pages/userPage/UserPage.jsx";
+import AgentPage from "./pages/agentPage/AgentPage.jsx";
 
 const App = () => {
   return (
-    <div>
-      Hi 
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/agent" element={<AgentPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
