06616a7cfb523cd40da24c55ed583bee1fbe2c3c
Shivam <sriyanshshivam1@gmail.com>
2024-11-30 03:50:12 +0530

    Complete base setup for App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,33 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { nanoid } from 'nanoid';
+import AgentDashboard from "./pages/agentPage/agentDashboard/AgentDashboard";
+import AgentTicketView from "./pages/agentPage/agentTicketView/AgentTicketView";
+import UserLandingPage from "./pages/userPage/userLandingPage";
+import UserCreateTicket from "./pages/userPage/userCreateTicket/UserCreateTicket";
+import UserDashboardView from "./pages/userPage/userDashboardView/UserDashboardView";
+import UserTicketView from "./pages/userPage/UserTicketView";
+import UserChat from "./components/userChat/UserChat";
+import AgentChat from "./components/agentChat/AgentChat";
+import HomePage from "./pages/homePage/HomePage";
 
 const App = () => {
+  const unique_id = nanoid(5);
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
+        <Route path="/*" element={<HomePage />} />
+      {/* Admin routes */}
+        <Route path="/admin/dashboard" element={<AgentDashboard />} />
+        <Route path="/admin/ticket/:id" element={<AgentTicketView />} />
+        <Route path="/admin/chat/:id" element={<AgentChat />} />
 
-      </Routes>
-    </BrowserRouter>
+      {/* User routes */}
+        <Route path="/user/*" element={<UserLandingPage userId={unique_id}/>} />
+        <Route path="/user/create" element={<UserCreateTicket userId={unique_id}/>} />
+        <Route path="/user/dashboard/*" element={<UserDashboardView userId={unique_id}/>} />
+        <Route path="/user/ticket/:id" element={<UserTicketView userId={unique_id}/>} />
+        <Route path="/user/chat/:id" element={<UserChat />} />
+    </Routes>
   );
-};
+}
 
 export default App;
