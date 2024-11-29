af041e7d13602f12a07a812285428a16249dc9f8
Shivam <sriyanshshivam1@gmail.com>
2024-11-30 04:49:07 +0530

    Finish Homepage styling

--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -8,20 +8,31 @@ import "./HomePage.scss";
 const HomePage = (props) => {
   const unique_id = nanoid(5);
   return (
-    <div>
-      <h1 style={{ marginTop: "4%" }}>Company Customer Support</h1>
-      <ul>
-        <li>
-          <Link className="link-style" to="/agent/dashboard">
-            <h2>Admin Dashboard</h2>
-          </Link>
-        </li>
-        <li>
-          <Link className="link-style" to="/user/*">
-            <h2>User Login</h2>
-          </Link>
-        </li>
-      </ul>
+    <div className="homepage-background">
+      <div class="card">
+        <div class="bg"></div>
+        <div class="blob"></div>
+      </div>
+      <div className="homepage-content">
+        <h1 className="homepage-header">Bolt</h1>
+        <p className="header-subheading">Your one stop solution</p>
+        <div className="list-div">
+          <div className="list-heading">Login as: </div>
+          <ul>
+            <li>
+              <Link className="link-style" to="/agent/dashboard">
+                <h2 className="link-header">Agent </h2>
+              </Link>
+            </li>
+            <li>
+              <Link className="link-style" to="/user/*">
+                <h2 className="link-header">User </h2>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       {/* Use Routes to handle navigation */}
       <Routes>
         <Route path="/agent/dashboard" element={<AgentDashboardView />} />
